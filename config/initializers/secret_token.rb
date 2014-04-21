333f465f87188102d27a1a8cc9e572c1ed9fd1a2
Berta-G <berta.gutierrez@gmail.com>
2014-04-21 16:44:43 +0100

    first commit - gems,cucumber & rpsec set

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Workshare::Application.config.secret_key_base = 'bd47d76e0fc6c740a44457424b454010042d89798ea49548e4deef1a110db0a2a673e5760f542962fabc3f60566b09c460fca26c379bde417e984a146c37e176'
